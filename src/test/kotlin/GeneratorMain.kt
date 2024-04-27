7e10dfa3a63332cefcd2d06593cb2be7dc1bac13
Terairk <terairkgaming@gmail.com>
2024-04-27 23:48:19 +0100

    Move code around from the Test to GeneratorMain. Clean up Generator code as it was verbose and unwieldly.

--- src/test/kotlin/GeneratorMain.kt
+++ src/test/kotlin/GeneratorMain.kt
@@ -1,21 +1,55 @@
+import io.kotest.property.Arb
+import io.kotest.property.arbitrary.element
+import io.kotest.property.arbitrary.long
+import io.kotest.property.arbitrary.map
 import io.kotest.property.arbitrary.next
+import io.kotest.property.arbitrary.stringPattern
+import io.kotest.property.arbitrary.arbitrary
+import io.kotest.property.arbitrary.choose
+
 
 // used kotest to generate expressions for me, sometimes it stack overflows
-// if i use too many binaryExpressions though and it's kinda hard to verify the values using tests
+// if I use too many binaryExpressions though, and it's kinda hard to verify the values using tests
 // could be useful in the future though
 fun main() {
-    val gen1 = expressionGenerator()
+    val gen1 = expressionGenerator
     val parser = RecursiveParser()
 
-    var input = gen1.next()
-    println("input is $input")
-    println("Recursive Parser is ${parser.evaluateInput(input)}")
-    println(parser.parseFromString(input))
+    var input: String
 
     for (i in 0..10) {
         input = gen1.next()
-        println("input: $i is $input")
-        println("Recursive Parser: $i is ${parser.evaluateInput(input)}")
+        println("input #$i: input is $input")
+        println("Recursive Parser #$i: value is ${parser.evaluateInput(input)}")
         println(parser.parseFromString(input))
+        println("=".repeat(32))
     }
 }
+
+val numberGenerator: Arb<String> = Arb.long(0,300).map { it.toString() }
+
+val signGenerator = Arb.element("", "-")
+val operationGenerator = Arb.element("+", "-", "*")
+val elementGenerator = Arb.stringPattern("element")
+
+val constantExpressionGenerator: Arb<String> =
+    arbitrary {
+        val sign = signGenerator.bind()
+        val number = numberGenerator.bind()
+        "$sign$number"
+    }
+
+val binaryExpressionGenerator: Arb<String> =
+    arbitrary {
+        val left = expressionGenerator.bind()
+        val operation = operationGenerator.bind()
+        val right = expressionGenerator.bind()
+        "($left $operation $right)"
+    }
+
+val expressionGenerator: Arb<String> =
+        Arb.choose(
+            2 to elementGenerator,
+            40 to constantExpressionGenerator,
+            30 to binaryExpressionGenerator,
+        )
